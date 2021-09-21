706eebddb7904e38aed5d1b6fefc7a5f3ff3f6a6
TotoroDavid <davidtoro65@hotmail.com>
2021-09-21 10:56:46 -0500

    contact page done!!

---
+++ src/pages/ContactMe.js
@@ -0,0 +1,61 @@
+import '../components/Contact/style.css'
+import Footer from '../components/Home/Footer'
+
+const ContactMe = () => {
+    return (
+        <>
+            <div className='container pt-4 contact_title'>
+                <h2>Say hello.</h2>
+            </div>
+            <div className='container p-0'>
+                <div className='row'>
+                    <div className='col-md-6 contact_cont p-0 m-0'>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='col-md-12 p-0'>
+                                    <h4 className='pt-5 pb-4'>
+                                        hello@ <br />
+                                        davidtoro65@hotmail.com
+                                    </h4>
+                                </div>
+                                <div className='col-md-12'>
+                                    <p>
+                                        For any enquiries, or just to say hello, get in touch and contact me.
+                                        If you’d like to send me a message, I can be reached through various ways! If you prefer email, simply fill out the form below or send me a message directly at
+                                    </p>
+                                </div>
+                                <div className='col-md-6 d-flex flex-column'>
+                                    <p className='d-flex justify-content-center pt-4 gray'>Phone</p>
+                                    <p className='d-flex justify-content-center text-muted'>+57 (315) 294-7533</p>
+                                </div>
+                                <div className='col-md-6 d-flex flex-column'>
+                                    <p className='d-flex justify-content-center pt-4 gray'>Enquiries</p>
+                                    <p className='d-flex justify-content-center text-muted'>davidtoro65@hotmail.com</p>
+                                </div>
+                                <div className='d-flex justify-content-center gray pt-3'>Follow me</div>
+                                <div className='col-md-12 d-flex flex-row justify-content-center'>
+                                    <a className='p-3 text-secondary text-decoration-none' href="">Tw</a>
+                                    <a className='p-3 text-secondary text-decoration-none' href="">Lk</a>
+                                    <a className='p-3 text-secondary text-decoration-none' href="">In</a>
+                                    <a className='p-3 text-secondary text-decoration-none' href="">Git</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 p-0'>
+                        <img
+                            src="https://avatars.githubusercontent.com/u/69094048?v=4" alt=""
+                            className='container p-0' />
+                    </div>
+                </div>
+                <br />
+                <br />
+                <br />
+            </div>
+            <Footer />
+        </>
+
+    )
+}
+
+export default ContactMe
--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Switch, Route }
  */
 /**Navbar and menu */ from "react-router-dom"
 import About from "../pages/About"
+import ContactMe from "../pages/ContactMe"
 import Home from "../pages/Home"
 import Navbar from '../pages/Navbar'
 import { Work } from "../pages/Work"
@@ -20,6 +21,7 @@ const Portfolio = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/work" exact component={Work} />
                 <Route path="/about" exact component={About} />
+                <Route path="/contact" exact component={ContactMe} />
             </Switch>
         </Router>
     )
